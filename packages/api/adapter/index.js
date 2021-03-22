8c9608ec7b77c6a7fee79fcb7353072f266b5f19
Jonattan Infante Vargas <jonattan@booklick.co>
2021-03-21 19:45:12 -0500

    Se Agrega EndPoint para realizar Login

---
+++ packages/api/adapter/index.js
@@ -0,0 +1,7 @@
+const healthyChecks = require("./healthyChecks");
+const user = require("./user");
+
+module.exports = {
+  healthyChecks,
+  user,
+};
--- packages/user-model/src/useCases/user.js
+++ packages/user-model/src/useCases/user.js
@@ -1,9 +1,12 @@
 
-const UserModel = require("../entities/user");
+const env = require('../../config/enviroment')
+const  setupUserModel = require("../entities/user");
 
 async function createdUser(UserData) {
+  
+
     try {
-        const userInstance = await UserModel.create({
+        const userInstance = await setupUserModel(env).create({
           firstName: UserData.firstName,
           lastName: UserData.lastName,
           mail: UserData.mail,
@@ -12,8 +15,33 @@ async function createdUser(UserData) {
 
         return userInstance;
     } catch (error) {
-        console.log(error.message);
+        throw new Error(error.message);
     }
 }
 
-module.exports = { createdUser };
+async function login(UserData) {
+  try {
+    const userInstance = await setupUserModel(env).findAll({
+      where: {
+        mail: UserData.mail
+      }
+    });
+
+    if (userInstance.length == 0) {
+      throw new Error("The User don't exist");
+    } else if (userInstance[0].password == UserData.password) {
+      return {
+        firstName: userInstance[0].firstName,
+        lastName: userInstance[0].lastName,
+        mail: userInstance[0].mail,
+        isVerify: userInstance[0].isVerify,
+      };
+    } else {
+      throw new Error("Password Invalid");
+    }
+
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+module.exports = { createdUser, login };
