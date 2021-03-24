9f640b9d8f2ee81ac7e1505f80a576a2deffeed2
Jonattan Infante Vargas <jonattan@booklick.co>
2021-03-23 22:25:31 -0500

    Fix load activities

--- packages/app/public/js/activity.js
+++ packages/app/public/js/activity.js
@@ -13,18 +13,19 @@ function obtenerActividad (){
       
       fetch("http://localhost:3000/activities", requestOptions)
         .then(response => response.text())
-        .then(result => {console.log(result) 
+        .then(result => {
+            console.log(JSON.parse(result)); 
             const div = document.querySelector(".padre")
-
-            for (let index = 0; index < result.length; index++) {
+            let data = JSON.parse(result).data;
+            for (let index = 0; index < data.length; index++) {
                 let div1 = document.createElement("div");
                 div1.className = "col-lg-3 col-md-6" 
                 let div2 = document.createElement("div");
                 div2.className = "service-item"
                 let h3 = document.createElement("h3");
-                h3.textContent = result[index].title
+                h3.textContent = data[index].title
                 let p = document.createElement("p");
-                p.textContent = result[index].description
+                p.textContent = data[index].description
                 let a = document.createElement("a");
                 a.id = "unirse"
                 a.style = "padding: 2px; background-color: rgb(70, 143, 238); color: white;"
@@ -34,9 +35,10 @@ function obtenerActividad (){
                 div2.appendChild(p)
                 div2.appendChild(a)
                 div1.appendChild(div2)
-
-
+                div.appendChild(div1);
             } 
+
+
 
 
         })
