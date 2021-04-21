50a010c56b8177345aa7059a0a0ca0961eef6663
Juan David <juandavid@192.168.1.104>
2021-04-21 07:49:23 -0500

    actulizacion ruta psicologos

--- packages/api/delivery/http/router/psicologos/index.js
+++ packages/api/delivery/http/router/psicologos/index.js
@@ -1,8 +1,8 @@
 const { regPsicologos } = require("../../../../adapter");
 
 async function registroPsicologosRute(fastify) {
-  fastify.post("/registerPsicologos", user.createUserPsicologos);
-  fastify.post("/login", user.loginUser);
+  fastify.post("/registerPsicologos", regPsicologos.createUserPsicologos);
+  fastify.post("/login", regPsicologos.loginUser);
 }
 
 module.exports = linkRouter;
