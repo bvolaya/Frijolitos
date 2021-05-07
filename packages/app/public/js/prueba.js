dce044e718d864719ed8f9a37271a59b3d64bdba
juliethGomez <julieth.gomez@correo.usa.edu.co>
2021-05-06 21:53:18 -0500

    roles

---
+++ packages/app/public/js/prueba.js
@@ -0,0 +1,84 @@
+let actividades = [
+    {
+        id:1,
+        "title": "Escuela de Familias",
+        image: "../img/bici.jpg",
+	    "direccion": "Cl. 63 #45-10",
+	    "fecha": "16/05/2021",
+        "description": "ejemploDescripcion",
+    },
+    {
+        id:2,
+        "title": "Escuela de Familias",
+        image: "../img/bici.jpg",
+	    "direccion": "Cl. 63 #45-10",
+	    "fecha": "16/05/2021",
+        "description": "ejemploDescripcion",
+    },
+    {
+        id:3,
+        "title": "Escuela de Familias",
+        image: "../img/bici.jpg",
+	    "direccion": "Cl. 63 #45-10",
+	    "fecha": "16/05/2021",
+        "description": "ejemploDescripcion",
+    }
+]
+
+
+
+if (document.addEventListener){
+    window.addEventListener('load',insertActivities(actividades),false);
+
+} else {
+    window.attachEvent('onload',insertActivities(actividades));
+}
+
+
+
+function insertActivities(data) {
+    const div = document.querySelector(".container")
+    let divFather = document.createElement("div")
+        divFather.className = "cardbox";
+    if (data.length > 0){
+        for (let index = 0; index < data.length; index++) {
+            let div1 = document.createElement("div");
+            div1.className = "card sombra"
+            let div2 = document.createElement("div");
+            let div3 = document.createElement("div");
+            div3.className = "actividadParticipante"
+	        let h3 = document.createElement("h3");
+            h3.textContent = data[index].title
+            let img = document.createElement("img");
+            img.src = data[index].image
+	        let h4 = document.createElement("h4");
+            h4.textContent = data[index].direccion
+	        let h5 = document.createElement("h5");
+            h5.textContent = data[index].fecha
+            let p = document.createElement("p");
+            p.textContent = data[index].description
+            let button = document.createElement("button");
+            button.className = "btn"
+            button.textContent = "UNIRSE"
+            button.onclick =  function() {suscribe(event)}
+
+            div3.appendChild(h3)
+            div3.appendChild(img)
+	        div3.appendChild(h4)
+            div3.appendChild(h5)
+            div3.appendChild(p)
+            div3.appendChild(button)
+            div2.appendChild(div3)
+            div1.appendChild(div2)
+            divFather.appendChild(div1);
+            div.appendChild(divFather);
+        }
+    }else{
+            let p = document.createElement("p");
+            p.textContent = "No encontramos actividades para mostrarte 🤦‍♂️"
+            divFather.appendChild(p);
+            div.appendChild(divFather);
+    }
+
+
+}
