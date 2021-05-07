c074b0a615d36f759a5ed496491f4e71fcbb97d2
Jonattan Infante Vargas <jonattan@booklick.co>
2021-05-06 21:55:25 -0500

    Se agrega rutas staticas

--- packages/app/server.js
+++ packages/app/server.js
@@ -3,6 +3,16 @@ const app = express();
 const port = 5000;
 
 // Setting up the public directory
-app.use(express.static("public"));
+app.use('/css',express.static(__dirname +'/public/css'));
+app.use('/js',express.static(__dirname +'/public/js'));
+app.use('/img',express.static(__dirname +'/public/img'));
+app.use('/font',express.static(__dirname +'/public/font'));
+app.get('/', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/index.html')
+})
+
+app.get('/event', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/tableroDeEventos.html')
+})
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
