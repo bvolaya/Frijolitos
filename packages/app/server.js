9ce83ea9978780a5231c355dd6f1d5b2e9ab519b
Jonattan Infante Vargas <jonattan@booklick.co>
2021-05-12 23:25:08 -0500

    Se agregan todas las rutas del frontend

--- packages/app/server.js
+++ packages/app/server.js
@@ -7,12 +7,57 @@ app.use('/css',express.static(__dirname +'/public/css'));
 app.use('/js',express.static(__dirname +'/public/js'));
 app.use('/img',express.static(__dirname +'/public/img'));
 app.use('/font',express.static(__dirname +'/public/font'));
+
+// Rutas de Inicio
 app.get('/', (request,response)=>{
     response.sendFile(__dirname +'/public/html/index.html')
 })
+app.get('/contact', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/contacto.html')
+})
+app.get('/activities', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/actividades.html')
+})
+app.get('/gallery', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/gallery.html')
+})
 
-app.get('/event', (request,response)=>{
-    response.sendFile(__dirname +'/public/html/tableroDeEventos.html')
+// Rutas de login
+app.get('/login', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/login.html')
+})
+// Rutas de registro
+app.get('/register', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/registroCrador.html')
+})
+
+app.get('/register/psychology', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/registroCrador.html')
+})
+
+// Rutas de eventos
+app.get('/events', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/EventosParticipante.html')
+})
+
+app.get('/event/create', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/crearEventos.html')
+})
+// Rutas de perfiles
+app.get('/profile', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/PerfilParticipante.html')
+})
+app.get('/profile/psychology', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/perfilPsicologo.html')
+})
+
+// Rutas de dashboard
+app.get('/dashboard', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/dashParticipante.html')
+})
+
+app.get('/dashboard/psychology', (request,response)=>{
+    response.sendFile(__dirname +'/public/html/dashPerfilPsicologo.html')
 })
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
