fa674c8f0a30e577d4d6617f525220d4cef489e2
Juan David <juandavid@192.168.1.107>
2021-05-18 23:25:16 -0500

    agregar hora al formulario de evento y mod login

--- packages/user-model/src/useCases/user.js
+++ packages/user-model/src/useCases/user.js
@@ -1,21 +1,21 @@
 
-const  setupUserModel = require("../entities/user");
+const setupUserModel = require("../entities/user");
 
 async function createdUser(UserData) {
-  
 
-    try {
-        const userInstance = await setupUserModel().create({
-          firstName: UserData.firstName,
-          lastName: UserData.lastName,
-          mail: UserData.mail,
-          password: UserData.password,
-        });
 
-        return userInstance;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+  try {
+    const userInstance = await setupUserModel().create({
+      firstName: UserData.firstName,
+      lastName: UserData.lastName,
+      mail: UserData.mail,
+      password: UserData.password,
+    });
+
+    return userInstance;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 async function login(UserData) {
@@ -31,6 +31,8 @@ async function login(UserData) {
         "mail",
         "isVerify",
         "password",
+        "role",
+        "profile"
       ],
     });
 
@@ -38,17 +40,37 @@ async function login(UserData) {
     if (userInstance.length == 0) {
       throw new Error("The User don't exist");
     } else if (userInstance[0].password == UserData.password) {
+      var profile = {}
+      if (UserData.role == "psychology") {
+        profile = {
+          nickName: userInstance[0].nickName,
+          description: userInstance[0].description,
+          img: userInstance[0].img
+        }
+      } else if (UserData.role == "participant") {
+        profile = {
+          cc: userInstance[0].cc,
+          urlcc: userInstance[0].urlcc,
+          profesionalcard: userInstance[0].profesionalcard,
+          urlcard: userInstance[0].urlcard,
+          img: userInstance[0].img,
+          direction: userInstance[0].direction,
+          phone: userInstance[0].phone,
+          yeargraduation: userInstance[0].yeargraduation
+        }
+      }
       return {
-        id:userInstance[0].id,
-        firstName: userInstance[0].firstName,
-        lastName: userInstance[0].lastName,
-        mail: userInstance[0].mail,
-        isVerify: userInstance[0].isVerify,
-      };
+        "id": userInstance[0].id,
+        "firstName": userInstance[0].firstName,
+        "lastName": userInstance[0].lastName,
+        "mail": userInstance[0].mail,
+        "isVerify": userInstance[0].isVerify,
+        "role": userInstance[0].role,
+        "profile": profile
+      }
     } else {
       throw new Error("Password Invalid");
     }
-
   } catch (error) {
     throw new Error(error.message);
   }
