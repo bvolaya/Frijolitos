6c345e23638b40ef1a10ef46868089554b437036
brandon Olaya <brandon.olaya@correo.usa.edu.co>
2021-05-21 21:59:50 -0500

    base de datos con alertas

--- packages/app/public/js/alertDescrip.js
+++ packages/app/public/js/alertDescrip.js
@@ -1,25 +1,48 @@
-function aleta(){
+function obtenerActividad (event) {
+    console.log("");
+    let id = event.path[3].getAttribute('id')
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    let idUser= JSON.parse(sessionStorage.getItem('user')).data.id
+    fetch(`http://localhost:3000/activities/${idUser}`, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            let data = JSON.parse(result).data;
+            data=data.filter(item=>item.id=id);
+            aleta(data)
+        })
+        .catch(error => console.log('error', error));
+}
+function suscribe(event) {
+    let idActivities = event.path[3].getAttribute('id')
+}
+
+function aleta(data){
     Swal.mixin({
-        //input: 'text', //puede ser text, number, email, password, textarea, select, radio
+        input: 'text', //puede ser text, number, email, password, textarea, select, radio
         confirmButtonText: 'Siguiente',
         showCancelButton: true,
         progressSteps: ['1', '2', '3','4','5','6','7']
     }).queue([
         {
-            imageUrl: '../img/teatro.jpg',
-        title: 'Teatro',
+        imageUrl: data[0].image,
+        title: data[0].title,
+        imageWidth: 350,
+        imageHeight: 200,
         },
         {
         title: 'Descripción',
-        text: 'Vamos comunicarno por medio de la actuacion.'
+        text: data[0].description,
         },
         {
         title: 'Autor de la actividad.',
-        text: 'Brenda Benitez'
+        text: 'Brenda Benitez',
         },
         {
             title: 'Lugar.',
-            text:'Suba'
+            text:'a'
         },
         {
             title:'Hora del evento.',
@@ -46,22 +69,17 @@ function aleta(){
         }
     });
 
-    // Swal.fire({
-    //     imageUrl: '../img/teatro.jpg',
-    //     title: 'Teatro',
-    //     text: 'Vamos al intereactuar y hacer actuacion',
-    //     text: 'Brenda Venites',
-    //     text: 'Centro',
-    //     text: 'Comunicacion',
-    //     text: '3:00p.m.',
-    //     text: 'cantidad de participantes: 8',
-    //     padding: '1rem',
-    //     timer: 20000,
-    //     imageWidth: 350,
-    //     imageHeight: 200,
-    //     imageAlt: 'A tall image',
-    //     confirmButtonText: 'Yes, delete it!'
-    // });
+//     Swal.fire({
+//         imageUrl: '../img/teatro.jpg',
+//         title: 'Teatro',
+//         text: 'Vamos al intereactuar y hacer actuacion '+'holas',
+//         padding: '1rem',
+//         timer: 1000,
+//         imageWidth: 350,
+//         imageHeight: 200,
+//         imageAlt: 'A tall image',
+//         confirmButtonText: 'Yes, delete it!'
+//     })
 };
 // $("#btn3").click(function(){
 //     Swal.fire({
