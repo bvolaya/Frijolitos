d5f335bcaea57740ce56886d25939f193a8817e9
Jonattan Infante Vargas <jonattan@booklick.co>
2021-05-25 01:30:49 -0500

    Fix de ruta del psicologo

--- packages/app/public/js/menu.js
+++ packages/app/public/js/menu.js
@@ -9,6 +9,8 @@ function menu(){
     let user = JSON.parse(sessionStorage.getItem('user')).data.rol
     if (user === 'psychology'){
         const urlProfile = document.querySelector(".profileUser")
+        const urlDashboard = document.querySelector(".dashboardUser")
         urlProfile.href='/profile/psychology'
+        urlDashboard.href='/dashboard/psychology'
     }
 }
