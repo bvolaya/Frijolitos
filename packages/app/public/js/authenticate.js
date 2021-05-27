9f0ccf46b1e521f0e07191b09d5a59d4b47c6309
Jonattan Infante Vargas <jonattan@booklick.co>
2021-05-26 22:00:53 -0500

    Se agrega fix the authenticacion en la ruta /login

--- packages/app/public/js/authenticate.js
+++ packages/app/public/js/authenticate.js
@@ -6,7 +6,10 @@ if (document.addEventListener){
 
 
 function authenticate (){
-    if (!sessionStorage.getItem('user')){
+    let path = location.pathname
+    if (!sessionStorage.getItem('user') && path !== '/login'){
         window.location.href = "http://localhost:5000/login"
+    }else if(sessionStorage.getItem('user') && path === '/login'){
+        window.location.href = "http://localhost:5000/events"
     }
 }
